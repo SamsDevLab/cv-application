2c070777d230cb818d4009d7f9d68ec3ba29c6f4
Sam Hadfield <sthtech@protonmail.com>
2025-12-31 17:59:54 -0600

    feat: import General.css - style General component

--- src/components/resume-builder/user-info-sections/general-info/General.jsx
+++ src/components/resume-builder/user-info-sections/general-info/General.jsx
@@ -10,8 +10,26 @@ General Info Component Requirements:
 • Submit Button (to update info)
 */
 
+import "./General.css";
+
 function General() {
-  return console.log("General Info");
+  return (
+    <div className="general-container">
+      <h3>General Info</h3>
+      <div>
+        <label htmlFor="name">Name: </label>
+        <input type="text" id="name" />
+      </div>
+      <div>
+        <label htmlFor="email">Email: </label>
+        <input type="text" id="email" />
+      </div>
+      <div>
+        <label htmlFor="phone">Phone: </label>
+        <input type="text" id="phone" />
+      </div>
+    </div>
+  );
 }
 
 export default General;
