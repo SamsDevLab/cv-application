6a259779115af57ebe3519e1102f89488e8bc3f6
Sam Hadfield <sthtech@protonmail.com>
2026-01-01 12:35:16 -0600

    feat: add Resume component to keep modularity
    Add Resume component to keep things modular. This structure was originally
    lumped into ResumePreview defeating the purpose of components

---
+++ src/components/resume-preview/Resume.jsx
@@ -0,0 +1,13 @@
+import "../../styles/Resume.css";
+
+function Resume() {
+  return (
+    <div className="resume">
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default Resume;
