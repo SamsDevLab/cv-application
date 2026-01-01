57f8080389d1c4acf9878b833b9524635d7493b7
Sam Hadfield <sthtech@protonmail.com>
2026-01-01 10:59:21 -0600

    refactor: rm main-container and its styles

--- src/components/resume-builder/ResumeBuilder.jsx
+++ src/components/resume-builder/ResumeBuilder.jsx
@@ -7,11 +7,9 @@ function ResumeBuilder() {
   return (
     <section className="resume-builder">
       <h2>SDL's Resume Builder</h2>
-      {/* <main className="main-container"> */}
       <General />
       <Education />
       <Experience />
-      {/* </main> */}
     </section>
   );
 }
