4324c907276c0ea6f4ac12b1c9a8db721e65160a
Sam Hadfield <sthtech@protonmail.com>
2026-01-01 16:09:31 -0600

    feat: add 'experience-container' section

--- src/components/resume-preview/Resume.jsx
+++ src/components/resume-preview/Resume.jsx
@@ -10,8 +10,29 @@ function Resume() {
         <span>(555) 555-5555</span>
       </header>
       <hr />
-      <main></main>
-      <footer></footer>
+      <main>
+        <section className="education-container">
+          <h2>Education</h2>
+          <ul>
+            <li>School: University of Louisville</li>
+            <li>Major: English Literature</li>
+            <li>Years attended: 2007-2011</li>
+          </ul>
+        </section>
+        <hr />
+        <section className="experience-container">
+          <h2>Experience</h2>
+          <ul>
+            <li>Company Name: CrowdStrike</li>
+            <li>Position Title: Software Developer</li>
+            <li>
+              Responsibilities: did a bunch of wanking and things while I worked
+              here. Didn't hit quota once.
+            </li>
+            <li>Dates Employed: </li>
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
