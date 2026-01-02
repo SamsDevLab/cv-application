608e208f6c11fc00e888bd8f21319c3412c57f1b
Sam Hadfield <sthtech@protonmail.com>
2026-01-02 16:27:31 -0600

    refactor: replace date divs with 'DateRangeInput'

--- src/components/resume-builder/user-info-sections/Education.jsx
+++ src/components/resume-builder/user-info-sections/Education.jsx
@@ -1,41 +1,25 @@
-/*
-Education Component Requirements:
-• School/University
-• Major
-• Dates attended
-
-• Add Button
-• Delete Button
-• Edit Button (to change info)
-• Submit Button (to update info)
-*/
-
 import "../../../styles/SectionStyles.css";
+import FormElement from "./FormElement.jsx";
+import DateRangeInput from "./DateRangeInput.jsx";
+import ButtonContainer from "./ButtonContainer.jsx";
 
 function Education() {
   return (
     <div className="section-container">
       <h3>Education</h3>
-      <div className="input-container">
-        <label htmlFor="school-name">School Name: </label>
-        <input type="text" id="school-name" />
-      </div>
-      <div className="input-container">
-        <label htmlFor="major">Major: </label>
-        <input type="text" id="major" />
-      </div>
-      <div className="input-container">
-        <div className="date-container">
-          <div className="from-container">
-            <label htmlFor="from">From: </label>
-            <input type="date" id="from" />
-          </div>
-          <div className="to-container">
-            <label htmlFor="to">To: </label>
-            <input type="date" id="to" />
-          </div>
-        </div>
-      </div>
+      <FormElement
+        inputType="text"
+        inputId="school-name"
+        inputLabelText="School Name"
+        changeFn
+      />
+      <FormElement
+        inputType="text"
+        inputId="major"
+        inputLabelText="Major"
+        changeFn
+      />
+      <DateRangeInput />
       <div className="button-container">
         <button>Edit</button>
         <button>Submit</button>
