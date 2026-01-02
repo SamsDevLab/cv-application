0e59c76e20ecc35c5495701b6eeca68c99e8bf49
Sam Hadfield <sthtech@protonmail.com>
2026-01-02 16:58:10 -0600

    feat: add props to DateRangeInput
    Add props to DateRangeInput so that it can accept helper fns to change
    the 'From' and 'To' dates

--- src/components/resume-builder/user-info-sections/DateRangeInput.jsx
+++ src/components/resume-builder/user-info-sections/DateRangeInput.jsx
@@ -1,14 +1,14 @@
-function DateRangeInput() {
+function DateRangeInput({ changeFromDateFn, changeToDateFn }) {
   return (
     <div className="input-container">
       <div className="date-container">
         <div className="from-container">
           <label htmlFor="from">From: </label>
-          <input type="date" id="from" />
+          <input type="date" id="from" onChange={changeFromDateFn} />
         </div>
         <div className="to-container">
           <label htmlFor="to">To: </label>
-          <input type="date" id="to" />
+          <input type="date" id="to" onChange={changeToDateFn} />
         </div>
       </div>
     </div>
