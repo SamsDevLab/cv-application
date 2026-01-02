db1c362b28d3b1207f3b8441d5c846054aa77a95
Sam Hadfield <sthtech@protonmail.com>
2026-01-02 14:29:06 -0600

    feat: import data/useState, set up state variable
    Import general info obj from data.js. Set up state variable, state setter
    function, and helper functions to update individiual properties

--- src/components/resume-builder/user-info-sections/General.jsx
+++ src/components/resume-builder/user-info-sections/General.jsx
@@ -1,34 +1,56 @@
-/*
-General Info Component Requirements:
-• Name
-• Email
-• Phone Number
+import "../../../styles/SectionStyles.css";
+import { generalData } from "../../../data";
+import { useState } from "react";
 
-• Add Button
-• Delete Button
-• Edit Button (to change info)
-• Submit Button (to update info)
+/*
+Pseudocode: 
+
+• Need to be able to update the values in generalInfo obj in realtime using 
+  setGeneralInfo fn
+  • Do this by passing in props
 */
 
-// Start here tomorrow - keep building out the wireframe with buttons before moving on to another component
-
-import "../../../styles/SectionStyles.css";
-
 function General() {
+  const [generalState, setGeneralState] = useState(generalData);
+
+  const handleNameChange = (event) => {
+    setGeneralState({ ...generalState, name: event.target.value });
+  };
+
+  const handleEmailChange = (event) => {
+    setGeneralState({ ...generalState, email: event.target.value });
+  };
+
+  const handlePhoneChange = (event) => {
+    setGeneralState({ ...generalState, phone: event.target.value });
+  };
+
   return (
     <div className="section-container">
       <h3>General Info</h3>
       <div className="input-container">
-        <label htmlFor="name">Name: </label>
-        <input type="text" id="name" />
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          onChange={(event) => handleNameChange(event)}
+        />
       </div>
       <div className="input-container">
         <label htmlFor="email">Email: </label>
-        <input type="email" id="email" />
+        <input
+          type="email"
+          id="email"
+          onChange={(event) => handleEmailChange(event)}
+        />
       </div>
       <div className="input-container">
         <label htmlFor="phone">Phone: </label>
-        <input type="tel" id="phone" />
+        <input
+          type="tel"
+          id="phone"
+          onChange={(event) => handlePhoneChange(event)}
+        />
       </div>
       <div className="button-container">
         <button>Edit</button>
