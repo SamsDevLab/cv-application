95aba536435d65a51d4ec8ae26d09cde90214f8c
Sam Hadfield <sthtech@protonmail.com>
2026-01-02 17:01:01 -0600

    feat: add helper fns and pass into DateRangeInput

--- src/components/resume-builder/user-info-sections/Education.jsx
+++ src/components/resume-builder/user-info-sections/Education.jsx
@@ -1,9 +1,31 @@
 import "../../../styles/SectionStyles.css";
+import { educationData } from "../../../data.js";
+import { useState } from "react";
 import FormElement from "./FormElement.jsx";
 import DateRangeInput from "./DateRangeInput.jsx";
 import ButtonContainer from "./ButtonContainer.jsx";
 
 function Education() {
+  const [educationState, setEducationState] = useState(educationData);
+
+  console.log(educationState);
+
+  const handleSchoolChange = (event) => {
+    setEducationState({ ...educationState, school: event.target.value });
+  };
+
+  const handleMajorChange = (event) => {
+    setEducationState({ ...educationState, major: event.target.value });
+  };
+
+  const handleFromDateChange = (event) => {
+    setEducationState({ ...educationState, from: event.target.value });
+  };
+
+  const handleToDateChange = (event) => {
+    setEducationState({ ...educationState, to: event.target.value });
+  };
+
   return (
     <div className="section-container">
       <h3>Education</h3>
@@ -11,15 +33,18 @@ function Education() {
         inputType="text"
         inputId="school-name"
         inputLabelText="School Name"
-        changeFn
+        changeFn={(event) => handleSchoolChange(event)}
       />
       <FormElement
         inputType="text"
         inputId="major"
         inputLabelText="Major"
-        changeFn
+        changeFn={(event) => handleMajorChange(event)}
       />
-      <DateRangeInput />
+      <DateRangeInput
+        changeFromDateFn={(event) => handleFromDateChange(event)}
+        changeToDateFn={(event) => handleToDateChange(event)}
+      />
       <ButtonContainer />
     </div>
   );
