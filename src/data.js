0ba190ed75148d1a37f2341072377f4726d779b5
Sam Hadfield <sthtech@protonmail.com>
2026-01-02 14:26:53 -0600

    feat: add 'data' file w/ objs of user info
    Create 'data' file which houses three objects of user info - general info
    about user (name, email, phone), educational background, and work
    experience.

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const generalData = {
+  name: "",
+  email: "",
+  phone: "",
+};
+
+export const educationState = {
+  0: {
+    school: "U of L",
+    major: "English",
+    startDate: "11/2007",
+    endDate: "05/2011",
+  },
+};
+
+export const experienceState = {
+  0: {
+    company: "CrowdStrike",
+    position: "Software Developer",
+    responsibilities: "Breaking stuff right before it ships end prod",
+    startDate: "11/2023",
+    endDate: "07/2024",
+  },
+};
