23a2872ac4620916eef007ea3048ccf9cd219385
Sam Hadfield <sthtech@protonmail.com>
2026-01-06 09:51:01 -0600

    feat: pass allData obj to Resume as a prop

--- src/components/resume-preview/ResumePreview.jsx
+++ src/components/resume-preview/ResumePreview.jsx
@@ -1,10 +1,11 @@
 import "../../styles/ResumePreview.css";
 import Resume from "./Resume";
+import { allData } from "../../data";
 
 function ResumePreview() {
   return (
     <section className="resume-preview">
-      <Resume />
+      <Resume props={allData} />
     </section>
   );
 }
