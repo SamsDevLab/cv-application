d5208c29c337b2971f7cc434c04c16d08b98995c
Sam Hadfield <sthtech@protonmail.com>
2026-01-06 09:52:06 -0600

    feat: destructure props to place data
    Destructure props so that each data obj has its own variable. Pass proper
    information from each section into HTML scaffold

--- src/components/resume-preview/Resume.jsx
+++ src/components/resume-preview/Resume.jsx
@@ -1,14 +1,18 @@
 import "../../styles/Resume.css";
 
-function Resume() {
+function Resume({ props }) {
+  const generalData = props.generalData;
+  const educationData = props.educationData;
+  const experienceData = props.experienceData;
+
   return (
     <div className="resume">
       <hr className="header-line" />
       <header>
         <section className="general-info-container">
-          <h1>Sam's Dev Lab</h1>
-          <address>samsdevlab@proton.me</address>
-          <span>(555) 555-5555</span>
+          <h1>{generalData.name}</h1>
+          <address>{generalData.email}</address>
+          <span>{generalData.phone}</span>
         </section>
       </header>
       <hr />
@@ -16,26 +20,30 @@ function Resume() {
         <section className="education-container">
           <h2>Education</h2>
           <div className="details-container">
-            <p>University of Louisville</p>
-            <p>Louisville, KY</p>
+            <p>{educationData.school}</p>
+            <p>{educationData.location}</p>
           </div>
           <div className="details-container secondary">
-            <p>BA - English Literature</p>
-            <p>2007-2011</p>
+            <p>{educationData.major}</p>
+            <p>
+              {educationData.from} – {educationData.to}
+            </p>
           </div>
         </section>
         <hr />
         <section className="experience-container">
           <h2>Experience</h2>
           <div className="details-container">
-            <p>CrowdStrike</p>
-            <p>Austin, TX</p>
+            <p>{experienceData.company}</p>
+            <p>{experienceData.location}</p>
           </div>
           <div className="details-container secondary">
-            <p>Software Developer</p>
-            <p>November 2023 - July 2024</p>
+            <p>{experienceData.title}</p>
+            <p>
+              {experienceData.from} – {experienceData.to}
+            </p>
           </div>
-          <p>65+ cold calls a day supporting three different sales reps</p>
+          <p>{experienceData.responsibilities}</p>
         </section>
       </main>
     </div>
