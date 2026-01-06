f8e89522ee7af668674322c5e9ece6b05a4e79a9
Sam Hadfield <sthtech@protonmail.com>
2026-01-06 09:47:24 -0600

    feat: export allData obj, update location in objs
    Place all data objects into allData obj and export to ResumePreview for
    more streamlined experience than importing all objects separately.
    
    Create 'location' prop in 'educationData' and 'experienceData' objs

--- src/data.js
+++ src/data.js
@@ -1,20 +1,24 @@
 export const generalData = {
-  name: "",
-  email: "",
-  phone: "",
+  name: "Sam'ss Dev Lab",
+  email: "samsdevlab@proton.me",
+  phone: "555-555-5555",
 };
 
 export const educationData = {
   school: "U of L",
-  major: "English",
-  from: "11/2007",
-  to: "05/2011",
+  location: "Louisville, KY",
+  major: "BA - English Literature",
+  from: "08 / 2007",
+  to: "05 / 2011",
 };
 
 export const experienceData = {
   company: "CrowdStrike",
+  location: "Austin, TX",
   title: "Software Developer",
-  responsibilities: "Breaking stuff right before it ships end prod",
-  from: "11/2023",
-  to: "07/2024",
+  from: "11 / 2023",
+  to: "07 / 2024",
+  responsibilities: "Breaking stuff right before it ships to prod",
 };
+
+export const allData = { generalData, educationData, experienceData };
