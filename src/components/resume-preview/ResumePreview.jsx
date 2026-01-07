6f6ebb5b9c54dc8e1e9d66194c95f5d7b2a9bfe3
Sam Hadfield <sthtech@protonmail.com>
2026-01-07 15:33:56 -0600

    refactor: place props into appropriate elements
    Exchange importing the data objects directly to pass them in as props
    to ResumePreview. This way, ResumeBuilder can share changes to data with
    ResumePreview

--- src/components/resume-preview/ResumePreview.jsx
+++ src/components/resume-preview/ResumePreview.jsx
@@ -1,35 +1,18 @@
 import "../../styles/ResumePreview.css";
-import { useState } from "react";
-import { allData } from "../../data";
 import GeneralPreview from "./section-previews/GeneralPreview";
 import EducationPreview from "./section-previews/EducationPreview.jsx";
 import ExperiencePreview from "./section-previews/ExperiencePreview.jsx";
 
-function ResumePreview() {
-  const [resumeDataState, setResumeData] = useState(allData);
-
-  /*
-  Start here tomorrow (01/07)
-
-  Figure out:
-  • How to re-render this using updated allData object.
-  • Look into making these containers into components so you can
-  simplify this markup
-  */
-
-  const generalData = allData.generalData;
-  const educationData = allData.educationData;
-  const experienceData = allData.experienceData;
-
+function ResumePreview({ general, education, experience }) {
   return (
     <section className="resume-preview">
       <div className="resume">
         <hr className="header-line" />
         <header>
           <GeneralPreview
-            name={generalData.name}
-            email={generalData.email}
-            phone={generalData.phone}
+            name={general.name}
+            email={general.email}
+            phone={general.phone}
           />
         </header>
         <hr />
@@ -37,23 +20,23 @@ function ResumePreview() {
           <section className="education-container">
             <h2>Education</h2>
             <EducationPreview
-              school={educationData.school}
-              location={educationData.location}
-              major={educationData.major}
-              fromDate={educationData.from}
-              toDate={educationData.to}
+              school={education.school}
+              location={education.location}
+              major={education.major}
+              fromDate={education.from}
+              toDate={education.to}
             />
           </section>
           <hr />
           <section className="experience-container">
             <h2>Experience</h2>
             <ExperiencePreview
-              company={experienceData.company}
-              location={experienceData.location}
-              title={experienceData.title}
-              fromDate={experienceData.from}
-              toDate={experienceData.to}
-              responsibilities={experienceData.responsibilities}
+              company={experience.company}
+              location={experience.location}
+              title={experience.title}
+              fromDate={experience.from}
+              toDate={experience.to}
+              responsibilities={experience.responsibilities}
             />
           </section>
         </main>
