58307e87eada52f9655c65eac087b02c6becdf7d
Sam Hadfield <sthtech@protonmail.com>
2026-01-07 15:31:00 -0600

    refactor: import data, add state, pass props
    Import object data to App.jsx, add state variable for each object, and
    pass state props to ResumePreview. Before, the objs were being called
    too low in the tree so ResumeBuilder and ResumePreview could not share
    updates. Fixing this.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-// import { useState } from "react";
 import "./App.css";
+import { useState } from "react";
+import { generalData } from "./data.js";
+import { educationData } from "./data.js";
+import { experienceData } from "./data.js";
 import ResumeBuilder from "./components/resume-builder/ResumeBuilder.jsx";
 import ResumePreview from "./components/resume-preview/ResumePreview.jsx";
 
 function App() {
+  const [generalState, setGeneralState] = useState(generalData);
+  const [educationState, setEducationState] = useState(educationData);
+  const [experienceState, setExperienceState] = useState(experienceData);
+
   return (
     <>
       <main className="app">
         <ResumeBuilder />
-        <ResumePreview />
+        <ResumePreview
+          general={generalState}
+          education={educationState}
+          experience={experienceData}
+        />
       </main>
     </>
   );
