1adb9933ffe071716a1e7346b1c03ae15fded0f0
Sam Hadfield <sthtech@protonmail.com>
2026-01-08 15:29:50 -0600

    refactor: Education to pass form data to App
    Refactor Education component by wrapping the <FormElement>s in a
    <form>, adding handleEducationFormDataState, and adding handleSubmit

--- src/components/resume-builder/user-info-sections/Education.jsx
+++ src/components/resume-builder/user-info-sections/Education.jsx
@@ -1,57 +1,58 @@
 import "../../../styles/SectionStyles.css";
-import { educationData } from "../../../data.js";
 import { useState } from "react";
+import { educationData } from "../../../data.js";
 import FormElement from "../elements-and-inputs/FormElement.jsx";
 import DateRangeInput from "../elements-and-inputs/DateRangeInput.jsx";
 
-function Education() {
-  const [educationState, setEducationState] = useState(educationData);
+function Education({ changeFn }) {
+  const [educationformDataState, setEducationFormDataState] =
+    useState(educationData);
 
-  const handleSchoolChange = (event) => {
-    setEducationState({ ...educationState, school: event.target.value });
+  const handleEducationFormDataState = (event) => {
+    const prop = event.target.id;
+
+    setEducationFormDataState({
+      ...educationformDataState,
+      [`${prop}`]: event.target.value,
+    });
   };
 
-  const handleLocationChange = (event) => {
-    setEducationState({ ...educationState, location: event.target.value });
-  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  const handleMajorChange = (event) => {
-    setEducationState({ ...educationState, major: event.target.value });
-  };
-
-  const handleFromDateChange = (event) => {
-    setEducationState({ ...educationState, from: event.target.value });
-  };
-
-  const handleToDateChange = (event) => {
-    setEducationState({ ...educationState, to: event.target.value });
+    changeFn(educationformDataState);
   };
 
   return (
     <div className="section-container">
       <h3>Education</h3>
-      <FormElement
-        inputType="text"
-        inputId="school-name"
-        inputLabelText="School Name"
-        changeFn={(event) => handleSchoolChange(event)}
-      />
-      <FormElement
-        inputType="text"
-        inputId="location"
-        inputLabelText="Location"
-        changeFn={(event) => handleLocationChange(event)}
-      />
-      <FormElement
-        inputType="text"
-        inputId="major"
-        inputLabelText="Major"
-        changeFn={(event) => handleMajorChange(event)}
-      />
-      <DateRangeInput
-        changeFromDateFn={(event) => handleFromDateChange(event)}
-        changeToDateFn={(event) => handleToDateChange(event)}
-      />
+      <form onSubmit={handleSubmit}>
+        <FormElement
+          inputType="text"
+          inputId="school"
+          inputLabelText="School Name"
+          changeFn={(event) => handleEducationFormDataState(event)}
+        />
+        <FormElement
+          inputType="text"
+          inputId="location"
+          inputLabelText="Location"
+          changeFn={(event) => handleEducationFormDataState(event)}
+        />
+        <FormElement
+          inputType="text"
+          inputId="major"
+          inputLabelText="Major"
+          changeFn={(event) => handleEducationFormDataState(event)}
+        />
+        <DateRangeInput
+          changeFromDateFn={(event) => handleEducationFormDataState(event)}
+          changeToDateFn={(event) => handleEducationFormDataState(event)}
+        />
+        <div className="button-container">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
