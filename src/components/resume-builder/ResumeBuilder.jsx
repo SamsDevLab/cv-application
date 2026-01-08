d8430072111f31acac7f1ec05fd821db6c672d4d
Sam Hadfield <sthtech@protonmail.com>
2026-01-08 16:43:47 -0600

    feat: pass experienceStateChangeFn to component

--- src/components/resume-builder/ResumeBuilder.jsx
+++ src/components/resume-builder/ResumeBuilder.jsx
@@ -3,13 +3,17 @@ import General from "./user-info-sections/General";
 import Education from "./user-info-sections/Education.jsx";
 import Experience from "./user-info-sections/Experience";
 
-function ResumeBuilder({ generalStateChangeFn, educationStateChangeFn }) {
+function ResumeBuilder({
+  generalStateChangeFn,
+  educationStateChangeFn,
+  experienceStateChangeFn,
+}) {
   return (
     <section className="resume-builder">
       <h2>SDL's Resume Builder</h2>
       <General changeFn={generalStateChangeFn} />
       <Education changeFn={educationStateChangeFn} />
-      <Experience />
+      <Experience changeFn={experienceStateChangeFn} />
     </section>
   );
 }
