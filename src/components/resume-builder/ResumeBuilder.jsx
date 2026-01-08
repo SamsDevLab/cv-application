e35830c483d4ecb6e604aff516fc1992538dedb2
Sam Hadfield <sthtech@protonmail.com>
2026-01-08 15:39:50 -0600

    feat: add educationStateChange fn to prop

--- src/components/resume-builder/ResumeBuilder.jsx
+++ src/components/resume-builder/ResumeBuilder.jsx
@@ -3,12 +3,12 @@ import General from "./user-info-sections/General";
 import Education from "./user-info-sections/Education.jsx";
 import Experience from "./user-info-sections/Experience";
 
-function ResumeBuilder({ generalStateChangeFn }) {
+function ResumeBuilder({ generalStateChangeFn, educationStateChangeFn }) {
   return (
     <section className="resume-builder">
       <h2>SDL's Resume Builder</h2>
       <General changeFn={generalStateChangeFn} />
-      <Education />
+      <Education changeFn={educationStateChangeFn} />
       <Experience />
     </section>
   );
