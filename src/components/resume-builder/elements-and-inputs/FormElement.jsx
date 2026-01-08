915f4c6fc008c8e2a80dade0d7b2705f8ee7be45
Sam Hadfield <sthtech@protonmail.com>
2026-01-08 14:43:29 -0600

    feat: add form validation attrs to input
    Add form validation attrs (minLength, maxLenght), etc. to <input> tags
    using props

--- src/components/resume-builder/elements-and-inputs/FormElement.jsx
+++ src/components/resume-builder/elements-and-inputs/FormElement.jsx
@@ -1,11 +1,27 @@
-function FormElement({ inputType, inputId, inputLabelText, changeFn }) {
+function FormElement({
+  inputType,
+  inputId,
+  inputLabelText,
+  minLength,
+  maxLength,
+  placeholder,
+  changeFn,
+}) {
   return (
     <div className="input-container">
       <label htmlFor={inputId}>{inputLabelText}: </label>
       {inputType === "textarea" ? (
-        <textarea id={inputId} onChange={changeFn}></textarea>
+        <textarea id={inputId} onChange={changeFn} required></textarea>
       ) : (
-        <input type={inputType} id={inputId} onChange={changeFn} />
+        <input
+          type={inputType}
+          id={inputId}
+          minLength={minLength}
+          maxLength={maxLength}
+          placeholder={placeholder}
+          onChange={changeFn}
+          required
+        />
       )}
     </div>
   );
